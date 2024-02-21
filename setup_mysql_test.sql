aa01cfd86ae7486d9df70e7750feab01c568b01e
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-21 12:03:41 +0000

    the implementation of task 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- Create database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create user if if doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on the database to the user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant select privilege on performance_schema to the user
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
