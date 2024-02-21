9607744bdf147d1f2efc7cf2638f7a23d6045834
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-02-21 11:50:46 +0000

    made some minor mistake in the naming that has now been rectified

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant all privileges on the database to the user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnd_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant select privilege on performance_schema to the user
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
