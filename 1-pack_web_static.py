b4706a8a0174eca48d2b40906851ac2cc1f23a96
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-03-11 21:20:12 +0000

    some minor changes, shebang

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Fabric script that generates a .tgz archive from the contents of the
 web_static folder of the AirBnB Clone repo using the do_pack function.
 """
-
 
 from fabric.api import local
 from datetime import datetime
