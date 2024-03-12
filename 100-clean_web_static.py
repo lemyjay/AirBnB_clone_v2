02e6df3fa03fcf4afe0c255584d59d9bc3ddceb4
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-03-12 13:21:35 +0000

    hopefully last change

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -16,21 +16,24 @@ def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static folder.
     """
-    # Create the folder 'versions' if it doesn't exist
-    local("mkdir -p versions")
+    try:
+        # Create the folder 'versions' if it doesn't exist
+        local("mkdir -p versions")
 
-    # Create the name of the archive with the current date and time
-    now = datetime.now()
-    file_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        now.year, now.month, now.day, now.hour, now.minute, now.second)
+        # Create the name of the archive with the current date and time
+        now = datetime.now()
+        file_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+            now.year, now.month, now.day, now.hour, now.minute, now.second)
 
-    # Compress the contents of the web_static folder into a .tgz archive
-    result = local("tar -cvzf {} web_static".format(file_name))
+        # Compress the contents of the web_static folder into a .tgz archive
+        result = local("tar -cvzf {} web_static".format(file_name))
 
-    # Check if the archive has been correctly generated
-    if result.failed:
+        # Check if the archive has been correctly generated
+        if result.failed:
+            return None
+        return file_name
+    except Exception:
         return None
-    return file_name
 
 
 def do_deploy(archive_path):
